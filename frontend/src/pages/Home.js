0fba35a2fb9200e91f218502da0418446d2f454d
travyozza <84102421+travyozza@users.noreply.github.com>
2022-08-24 16:16:37 +1200

    first commit

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h2> How to use SEED</h2>
+      <p>I am not sure what to put on the home page</p>
+      <p>A nice logo? Instructions?</p>
+    </div>
+  );
+};
+
+export default Home;

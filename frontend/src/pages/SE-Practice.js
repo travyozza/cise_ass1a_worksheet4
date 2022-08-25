11605d085645685a397c687b075c80e1fc5f3554
travyozza <84102421+travyozza@users.noreply.github.com>
2022-08-25 16:41:52 +1200

    final commit

--- frontend/src/pages/SE-Practice.js
+++ frontend/src/pages/SE-Practice.js
@@ -1,17 +1,17 @@
-import React from "react";
+import articles from "../dummydata/articles.js";
+import Styles from "../components/tablestyle.js";
+import Table from "../components/Evidencetable.js";
+import tablecolumns from "../components/tablecolumns.js";
+import Dropdown from "../components/Dropdown.js";
 
 const SEPractice = () => {
   return (
     <div>
       <h2>Select SE Practice to get evidence for the claimed benefits</h2>
-      <p>
-        There will be a way to select an SE practice from our SEED repository
-        here
-      </p>
-      <p>
-        Also the display table of evidence levels for different claims will show
-        here for the SE practice selected
-      </p>
+      <Dropdown />
+      <Styles>
+        <Table data={articles} columns={tablecolumns} />
+      </Styles>
     </div>
   );
 };
--- frontend/src/pages/Submit-Article.js
+++ frontend/src/pages/Submit-Article.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SubmissionForm from "../components/SubmissionForm.js";
 
 const SubmitArticle = () => {
   return (
@@ -8,14 +9,7 @@ const SubmitArticle = () => {
         This will be a form to allow submitters to submit an article for
         possible inclusion in our SEED repo.
       </p>
-      <p>
-        Also the display table of evidence levels for different claims will show
-        here for the SE practice selected
-      </p>
-      <p>
-        Also the display table of evidence levels for different claims will show
-        here for the SE practice selected
-      </p>
+      <SubmissionForm />
     </div>
   );
 };
